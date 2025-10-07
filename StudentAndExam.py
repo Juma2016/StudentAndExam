cf5c103e47e3b5cae7aa6625e96e88330cd963af
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-10-07 21:52:01 +0200

    add bearbeitung file version 2

--- StudentAndExam.py
+++ StudentAndExam.py
@@ -1,29 +1,61 @@
-print('Welcome in Student and exam Port')
-print('Please Choose one from the menu')
-print('1) Add Student')
-print('2)Add Exercise grading')
-print('3)View Student')
-print('4)Exercies')
+students=[] # dictionary
+exercise={} # to save all data about student
 
-choosen =int(input()) #user choose number as String and we use int to change it to Integer
+while True:
+    print('Welcome in Student and exam Portal')
+    print('Please Choose one from the menu')
+    print('1) Add Student')
+    print('2)Add Exercise grading')
+    print('3)View Exercise')
+    print('4)view Student')
+    print('0)Exit')
 
-if(choosen==1):
-    print('please Enter your first name:')
-    firstname=input()
-    print('please insert your Last Name: ')
-    lastName=input()
-    print('Enter your martikel number: ')
-    martikelNumber=input()
-    
-if(choosen==2):
-    print('Please add the exercise identification number')
-    idNumber=int(input())
-    print('please add the number of points for each student for that exercise')
+    choosen =int(input()) #user choose number as String and we use int to change it to Integer
 
-if(choosen==3):
-    print('please insert the exercise number')
-    exerciseNumber=int(input())
-if(choosen==4):
-    print('the data of Student......')
-else:
-    print('Wrong User input')
+    if(choosen==1):
+        print('please Enter Student first name:')
+        firstName=input()
+        print('please Enter Student Last Name: ')
+        lastName=input()
+        print('Enter Student martikel number: ')
+        martikelNumber=input()
+
+        students={"firstName":firstName,"lastName":lastName,"martikelNumber":martikelNumber}
+        
+
+    elif(choosen==2):
+        print('Please add the exercise identification number')
+        idNumber=int(input())
+        points={} # to save the points of every exam 
+        for student in students:
+            print('please add the number of points for each student for that exercise')
+            mark=int(input())
+            mark = input(f"Enter points for {student['first_name']} {student['last_name']}: ")
+            points[student['martikel']] = mark
+
+            exercise[idNumber]=points
+
+    elif(choosen==3):
+        if not exercise:
+            print('Not exercise')
+        else:
+            for idNumber,points in exercise.items():
+                print(f'Exercise {idNumber}')
+                for martikelNumber,score in points.items():
+                    name=next((f'{student["firstName"]}{student["lastName"]}'
+                               for student in students
+                                 if student['martikelNumber']== martikelNumber),
+                                    'unkown')
+                    print(f'{name}({martikelNumber}): {score} points')
+    elif(choosen==4):
+        if not students:
+            print('Unkown Student')
+        else:
+            for student in students:
+                print(f"{student['firstName']} {student['lastName']} (martikelNumber: {student['martikelNumber']})")
+    elif(choosen==0):
+        print('Good Bye :)')
+        break
+
+    else:
+        print('Wrong User input,try again')
