b1c7df943ec8de71d9cafee484108e3ea1e710b2
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-10-07 21:13:12 +0200

    add the Version 1 from python project

--- StudentAndExam.py
+++ StudentAndExam.py
@@ -1,7 +1,7 @@
 print('Welcome in Student and exam Port')
 print('Please Choose one from the menu')
 print('1) Add Student')
-print('2)Add Exmam')
+print('2)Add Exercise grading')
 print('3)View Student')
 print('4)Exercies')
 
@@ -14,5 +14,16 @@ if(choosen==1):
     lastName=input()
     print('Enter your martikel number: ')
     martikelNumber=input()
-if(choosen==2):
     
+if(choosen==2):
+    print('Please add the exercise identification number')
+    idNumber=int(input())
+    print('please add the number of points for each student for that exercise')
+
+if(choosen==3):
+    print('please insert the exercise number')
+    exerciseNumber=int(input())
+if(choosen==4):
+    print('the data of Student......')
+else:
+    print('Wrong User input')
