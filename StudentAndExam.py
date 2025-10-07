5c3bbdcce58dc327c22f0ec9c4a8acd38ec079e8
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-10-07 21:03:46 +0200

    add StartUp Files

---
+++ StudentAndExam.py
@@ -0,0 +1,18 @@
+print('Welcome in Student and exam Port')
+print('Please Choose one from the menu')
+print('1) Add Student')
+print('2)Add Exmam')
+print('3)View Student')
+print('4)Exercies')
+
+choosen =int(input()) #user choose number as String and we use int to change it to Integer
+
+if(choosen==1):
+    print('please Enter your first name:')
+    firstname=input()
+    print('please insert your Last Name: ')
+    lastName=input()
+    print('Enter your martikel number: ')
+    martikelNumber=input()
+if(choosen==2):
+    
